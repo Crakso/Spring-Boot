4e0aa1c46173746d0b75a27e06a3a2d8e892538e
Neeraj <neerajbisht8126@gmail.com>
2026-02-18 10:41:45 +0530

    Add some admin services.

--- HospitalManagementSystem/hospitalmanagementsystem/src/main/java/com/hospitalmanagementsystem/hospitalmanagementsystem/Controller/AdminController.java
+++ HospitalManagementSystem/hospitalmanagementsystem/src/main/java/com/hospitalmanagementsystem/hospitalmanagementsystem/Controller/AdminController.java
@@ -1,9 +1,11 @@
 package com.hospitalmanagementsystem.hospitalmanagementsystem.Controller;
 
 import com.hospitalmanagementsystem.hospitalmanagementsystem.Entity.Appointment;
+import com.hospitalmanagementsystem.hospitalmanagementsystem.Entity.Doctor;
 import com.hospitalmanagementsystem.hospitalmanagementsystem.Entity.Patient;
 import com.hospitalmanagementsystem.hospitalmanagementsystem.Entity.Type.DoctorRegisterDTO;
 import com.hospitalmanagementsystem.hospitalmanagementsystem.Entity.Type.UpdateRequestDTO;
+import com.hospitalmanagementsystem.hospitalmanagementsystem.Service.AdminService;
 import com.hospitalmanagementsystem.hospitalmanagementsystem.Service.AppointmentServices;
 import com.hospitalmanagementsystem.hospitalmanagementsystem.Service.DoctorService;
 import com.hospitalmanagementsystem.hospitalmanagementsystem.Service.PatientService;
@@ -21,6 +23,7 @@ public class AdminController {
     private final PatientService patientService;
     private final DoctorService doctorService;
     private final AppointmentServices appointmentServices;
+    private final AdminService adminService;
 
 
     @GetMapping("get-patients")
@@ -45,10 +48,7 @@ public class AdminController {
         return patientService.updatePatientById(id, patient);
     }
 
-    @PostMapping("promote-To-Doc/{doctorId}")
-    public ResponseEntity<String> onBoardToDoctor(@RequestBody DoctorRegisterDTO doctor, @PathVariable Long doctorId){
-        return doctorService.promoteToDoctor(doctor,doctorId);
-    }
+
 
     // Manage Appointments.
 
@@ -74,9 +74,29 @@ public class AdminController {
         return doctorService.removeDoctor(doctorId);
     }
 
+    @PostMapping("promote-To-Doc/{doctorId}")
+    public ResponseEntity<String> onBoardToDoctor(@RequestBody DoctorRegisterDTO doctor, @PathVariable Long doctorId){
+        return doctorService.promoteToDoctor(doctor,doctorId);
+    }
+
+    @GetMapping("doctor/{id}")
+    public ResponseEntity<Doctor> getDoctorById(@PathVariable Long id){
+        return doctorService.getDoctorById(id);
+    }
 
 
 
+    //Manage ADMIN
+
+    @PostMapping("promote/{id}")
+    public ResponseEntity<String> promoteToAdmin(Long id){
+        return adminService.promoteUserToAdmin(id);
+    }
+
+    @PostMapping("remove/{id}")
+    public ResponseEntity<String> demoteAdmin(Long id){
+        return adminService.demoteUserFromAdmin(id);
+    }
 
 
 
---
+++ HospitalManagementSystem/hospitalmanagementsystem/src/main/java/com/hospitalmanagementsystem/hospitalmanagementsystem/Service/AdminService.java
@@ -0,0 +1,48 @@
+package com.hospitalmanagementsystem.hospitalmanagementsystem.Service;
+
+
+import com.hospitalmanagementsystem.hospitalmanagementsystem.Entity.Type.RoleType;
+import com.hospitalmanagementsystem.hospitalmanagementsystem.Entity.User;
+import com.hospitalmanagementsystem.hospitalmanagementsystem.Repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableMethodSecurity
+public class AdminService {
+
+    private final UserRepository userRepository;
+
+    public AdminService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> promoteUserToAdmin(Long id) {
+        try {
+            User user = userRepository.findById(id).orElse(null);
+            if (user == null) return new ResponseEntity<>("Invalid User.", HttpStatus.NOT_FOUND);
+            user.getRoles().add(RoleType.ADMIN);
+            userRepository.save(user);
+            return new ResponseEntity<>("User promoted successfully.", HttpStatus.OK);
+        }catch (Exception e) {
+            throw  new RuntimeException(e);
+        }
+    }
+
+    public ResponseEntity<String> demoteUserFromAdmin(Long id) {
+        try{
+            User user = userRepository.findById(id).orElse(null);
+            if(user==null) return new ResponseEntity<>("Invalid User",HttpStatus.NOT_FOUND);
+            user.getRoles().remove(RoleType.ADMIN);
+            userRepository.save(user);
+            return new ResponseEntity<>("Demoted Successfully.",HttpStatus.OK);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
